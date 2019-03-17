412ba1fd4346f041df45c64a49a6d96a3f532d55
Moritz Höppner <moritz.hoeppner@resourcify.de>
2019-03-17 17:23:16 +0100

    configured output managment, HtmlWebpack plugin and Cleanwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import './style.css';
-import './style.css';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
@@ -8,6 +8,11 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    var btn = document.createElement('button');
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
